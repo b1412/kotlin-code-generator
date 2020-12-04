b40ac8ec87b07f2b51198f0a0c33f9702ad55438
Leon Zhou <zhouleib1412@gmail.com>
2020-12-04 16:28:28 +1300

    update

--- src/main/kotlin/com/github/b1412/generator/task/MultipleTask.kt
+++ src/main/kotlin/com/github/b1412/generator/task/MultipleTask.kt
@@ -3,13 +3,13 @@ package com.github.b1412.generator.task
 import com.github.b1412.generator.task.processor.MultipleTaskProcessor
 
 open class MultipleTask(
-    folder: FilenameProcessor,
+    filePath: FilenameProcessor,
     templatePath: String,
     replaceFile: Boolean = true,
     entityExtProcessors: List<EntityExtProcessor> = listOf(),
     ignoreEntities: List<String> = listOf()
 ) : Task(
-    filePath = folder,
+    filePath = filePath,
     taskType = MultipleTaskProcessor(),
     templatePath = templatePath,
     replaceFile = replaceFile,
